343f510d72fd621e189bf2a53b97c7af0f987869
jin-li <lijin.thu@outlook.com>
2025-05-16 00:45:42 -0400

    feat(language): add language switcher to footer
    add a language switcher in footer to allow switch language manually
    
    - https://github.com/jin-li/jinli-home/issues/13

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
+import { RiTranslate2 } from "react-icons/ri"; // Import the translate icon
 import styles from "./Footer.module.scss";
 
 interface FooterProps {
@@ -8,13 +9,28 @@ interface FooterProps {
 }
 
 const Footer: React.FC<FooterProps> = ({ startYear, author }) => {
-  const { t } = useTranslation('ui');
+  const { t, i18n } = useTranslation('ui'); // Use the 'ui' namespace
   const currentYear = new Date().getFullYear();
+
+  // Function to toggle the language
+  const toggleLanguage = () => {
+    const newLanguage = i18n.language === 'en' ? 'zh' : 'en';
+    i18n.changeLanguage(newLanguage);
+  };
+
+  // Show the language to switch to
+  const nextLanguage = i18n.language === 'en' ? '简体中文' : 'English';
 
   return (
     <div className={styles.footer}>
+      <button onClick={toggleLanguage} className={styles.languageSwitcher}>
+        <span className={styles.icon}>
+          <RiTranslate2 size={20} />
+        </span>
+        <span className={styles.text}>{nextLanguage}</span>
+      </button>
       <span className={styles.copyright}>
-        {t("copyright", { startYear, currentYear, author})}
+        {t("copyright", { startYear, currentYear, author })}
       </span>
       <span className={styles.design}>
         {t("designed_by")}{" "}
